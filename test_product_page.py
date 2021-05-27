4a63c50f95edb06bd7a083090d27e450dad6987f
CaptainCollie <shadaraevd@gmail.com>
2021-05-27 18:47:03 +0800

    add pytest.ini and few small changes

--- test_product_page.py
+++ test_product_page.py
@@ -40,7 +40,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
-    page.open()
     page.add_product_to_basket()
 
 
